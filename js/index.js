6a2570aa23cd7b001ba6642b2a9eab7ce560c240
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-09-17 00:35:38 +0530

    refer

--- js/index.js
+++ js/index.js
@@ -1,11 +1,44 @@
 window.onload=function(){
-    console.log("this is index.js for just verify ");
+    // make a perfect
     var btnc = document.getElementById('btn-c');
     btnc.addEventListener('click', function(){
         console.log("clicked");
         document.location.href = '/contact.html';
     });
-   
+   // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var callBackBtn = document.getElementById("call-back");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+callBackBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//announcement section
+var annIcon = document.querySelector(".ann-icon i");
+setInterval(function(){
+    annIcon.classList.add("ani-icon");
+    setTimeout(function(){
+        annIcon.classList.remove("ani-icon");
+    },250);
+}, 500);
 }
 
 // var btns = document.getElementById('btn-s');
